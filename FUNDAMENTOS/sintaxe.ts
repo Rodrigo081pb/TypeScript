ffc33c0760820f0e97dba3dac19f8878372815a1
Rodrigo081pb <kauarodrigo1193@gmail.com>
2025-04-28 17:05:00 -0300

    Subindo

---
+++ FUNDAMENTOS/sintaxe.ts
@@ -0,0 +1,6 @@
+// Tipos de váriáveis
+
+var a = 'a' // De escopo global
+let b = 'b' // alterar o valor dessa variável ao longo do código
+const c = 'c' // Constante = imutavel e o valor dela vai ser sempre esse valor.
+
