c30131a0af73702d73fa69906e9920feb6c8b199
Rodrigo081pb <kauarodrigo1193@gmail.com>
2025-04-28 17:14:09 -0300

    Finalização da aula de sintaxe

--- FUNDAMENTOS/sintaxe.ts
+++ FUNDAMENTOS/sintaxe.ts
@@ -4,3 +4,28 @@ var a = 'a' // De escopo global
 let b = 'b' // alterar o valor dessa variável ao longo do código
 const c = 'c' // Constante = imutavel e o valor dela vai ser sempre esse valor.
 
+/* Tipos primitivos do Typescript
+
+os tipos primitivos abaixo tem 3 tipos que são os de 
+
+- String = caractere = letra
+- Number = inteiro = numero
+- Booleam = boleano = verdadeiro ou falso
+
+Ao declarar uma váriável é interessante a gente declarar o seu tipo para 
+aumentar a segurança do qual tipo essa variável vai aceitar e até mesmo defirnir o tipo
+delas por isso o uso do typescript.
+
+O que é interessante citar também é a versatilidade, que quando usada da maneira correta
+faz sentido usar. 
+
+também podemos verificar o uso do tipo any que é para aceitar qualquer tipo primitivo.
+
+*/
+
+let saudacao: string = "Hello World"
+let numero: number = 100
+let boleano: boolean = true
+
+let multi: string | number = 2
+multi = 'Rodrigo'
