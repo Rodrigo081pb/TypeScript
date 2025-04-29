112705d9a1baa35a8c48453464d20b1d8638a43b
Rodrigo081pb <kauarodrigo1193@gmail.com>
2025-04-29 01:27:21 -0300

    Subindo fechamento de modulo antes de falar de arrays

---
+++ FUNDAMENTOS/repeticoes.ts
@@ -0,0 +1,17 @@
+// Inicia um laço FOR que cria a variável 'i' começando em 0,
+// executa enquanto 'i' for menor que 5 e incrementa 'i' em 1 a cada iteração.
+for (let i = 0; i < 5; i++) {
+// Imprime o valor atual de 'i' no console.
+console.log(i);
+}
+
+// Declara uma variável 'num2' e atribui o valor inicial 2.
+let num2 = 2;
+
+// Inicia um laço WHILE que continuará enquanto 'num2' for menor que 10.
+while (num2 < 10) {
+// Imprime o valor atual de 'num2' no console.
+console.log(num2);
+// Incrementa 'num2' em 1 para avançar a condição do laço.
+num2++;
+}
