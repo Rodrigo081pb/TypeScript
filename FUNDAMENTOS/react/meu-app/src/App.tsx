64d9abfa41e2c387a880e1df4bfee5f1db7c11e8
Rodrigo081pb <kauarodrigo1193@gmail.com>
2025-05-04 22:33:05 -0300

    Subindo atualizações com o uso do styled/components

--- FUNDAMENTOS/react/meu-app/src/App.tsx
+++ FUNDAMENTOS/react/meu-app/src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react'
-import { Card } from "./components/Card";
+import styled from 'styled-components'
 import { Layout } from './components/Layout';
+
+/* 
+
+- O uso do styled components é muito bom para aplicação
+  partindo do principio que podemos usar ele como variável para determinar alguma tag
+  como mostrado abaixo
+
+  Uso de uma constante que leva o nome de box
+  onde eu chamo o styled e depois passo a tag e logo em seguida uso ` ` para poder declarar
+  os estilos.
+
+  logo em seguia chamo a variável através da tag como podem ver,
+  chamei de Box e envolvi ela visto que ela é uma div.
+
+  Isso é aplicado para muitas coisas e podem ser muito versátil
+
+*/
+
+const Box = styled.div`
+  background-color: orange
+`
 
 function App() {
   return (
 
     <Layout>
+      <Box>
+        <h1>Faça o login</h1>
+        <label htmlFor="emailInput">
+          Email
+        </label>
 
-      Hello World
+        <input type="email" />
 
-      <Card
-        id={1}
-        paragrafo='Náutico é o primeiro time nordestino ao ganhar um Mundial'
-        details='Kieza teve uma noite diferente dentro dos estádio dos aflitos'
-      />
+        <label htmlFor="passwordInput">
+          Senha
+        </label>
 
-      <Card
-        id={2}
-        paragrafo='Rodrigo é indiciado por ter a mulher mais linda do brasil'
-        details='Conheça o homem que tem a mulher mais linda do universo'
-      />
+        <input type="password"/>
 
+        <button>
+          Entrar
+        </button>
+      </Box>
     </Layout>
 
   );
